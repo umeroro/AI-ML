c7c9648e4b15d28ae0032043858e388c2390e65b
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-07-29 06:46:56 +0500

    Day02
    Escape Character & Strings

---
+++ Python Basics/day02.py
@@ -0,0 +1,15 @@
+# Strings
+
+course = "Python Fundamentals"
+print(course)
+print(len(course))
+print(course[7])
+print(course[0:7])
+print(course[0:])
+print(course[:0])
+print(course[19:0])
+
+# Escape Sequence
+
+course_with_Escape = "Python \n\"Fundamentals"
+print(course_with_Escape)
