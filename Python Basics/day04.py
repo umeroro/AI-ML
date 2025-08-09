c90c71345b2202bf649f803cbf3778febf7182db
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-08-09 17:14:20 +0500

    operators

---
+++ Python Basics/day04.py
@@ -0,0 +1,12 @@
+# Ternitory operator
+
+age = 10
+message = "eligible" if age >= 18 else "Not Eligible"
+print(message)
+
+# logical operators
+
+high_income = True
+good_credit = True
+if high_income and good_credit:
+    print("Eligible")
