e680bdf5e00f1cc0eafc88772a6a3f0fbcdb9107
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-08-10 17:05:25 +0500

    Day 04
    Short-circuit Evaluation, Chaining Comparison Operators, Loops

--- Python Basics/day04.py
+++ Python Basics/day04.py
@@ -5,8 +5,30 @@ message = "eligible" if age >= 18 else "Not Eligible"
 print(message)
 
 # logical operators
+# Short-circuit Evaluation
 
 high_income = True
 good_credit = True
-if high_income and good_credit:
+student = True
+if (high_income or good_credit) and not student:
     print("Eligible")
+else:
+    print("Not Eligible")
+
+# Chaining comparsion operators
+
+age = 22
+
+if 18 <= age < 65:
+    print("Eligible")
+else:
+    print("Not Eligible")
+
+# loops
+
+for i in range(10):
+    print(i)
+
+
+for i in range(0, 10, 2):
+    print(i, i*("."))
---
+++ Python Basics/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+10
+# message = "eligible" if age >= 18 else "Not Eligible"
+# print(message)
