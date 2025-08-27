8ff662b56380c6cb8316af41529c89e19e309ad6
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-08-27 12:08:30 +0500

    Practice concept one

--- Python Basics/conceptpractice1.py
+++ Python Basics/conceptpractice1.py
@@ -36,3 +36,33 @@ word = input("Enter Word : ")
 for w in range (word.__len__()):
     w+=1
 print(w)
+
+# List of Number
+
+list = [1,2,3,4,5]
+total = 0
+print(list[0])
+print(list[-1])
+for i in list:
+    total = total + i
+print(total)
+
+# Largest of Two Numbers
+
+a = int(input("Enter First Number : "))
+b = int(input("Enter Second Number : "))
+if a > b:
+    print(f"{a} is Larger")
+elif a == b:
+    print("Both are Equal")
+else:
+    print(f"{b} is Larger")
+
+# Simple Login System
+
+username = input("Enter username : ")
+password = input("Enter Password : ")
+if username == "ummorro" and password == "9876":
+    print("Login Succesfully")
+else:
+    print("Wrong Credentital! Try Again")
