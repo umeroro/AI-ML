8d4f1f2bedba20e3ea1533091070b8ac64abffa5
Muhammad Umer Amir <161680343+umeroro@users.noreply.github.com>
2025-08-30 05:50:38 -0700

    Concept Practice Part 2

---
+++ Python Basics/conceptpractice2.py
@@ -0,0 +1,24 @@
+# Factorial of a Number
+
+number = int(input("Enter Number : "))
+factorial = 1
+for num in range(1,number+1):
+    factorial = factorial * num
+print(f"Factorial of {number} : {factorial}")
+
+# Check Prime Number
+
+number = int(input("Enter Number : "))
+for num in range(2,number-1):
+    if number % num == 0:
+        print("Number is not Prime")
+        break
+    else: 
+        print("Number is Prime")
+        break
+    
+# Reverse a String
+
+word = input("Enter Word : ")
+for str in word:
+    print(str)
